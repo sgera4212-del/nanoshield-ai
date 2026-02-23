108bd9128bdc22549d4f4bce08f4eb221179aae7
sgera4212-del <sgera4212@gmail.com>
2026-02-23 23:09:29 +0530

    Update nanoshield_app.py

--- nanoshield_app.py
+++ nanoshield_app.py
@@ -1,4 +1,4 @@
-if st.button("Analyze Risk"):
+    if st.button("Analyze Risk"):
 
         input_df = pd.DataFrame({
             "Material": [material],
@@ -75,4 +75,3 @@ if st.button("Analyze Risk"):
         ax.set_title("Model Validation")
 
         st.pyplot(fig)
-
